388d3b42efd00690de271deab05f7ff25bec1294
Wesley Hatley <weshatley@gmail.com>
2019-09-21 03:01:51 -0400

    initial upload of Paper. still needs config to be demoable.

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+import AuthLayout from "layouts/Auth.jsx";
+import AdminLayout from "layouts/Admin.jsx";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/paper-dashboard.scss?v=1.1.0";
+import "assets/demo/demo.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/auth" render={props => <AuthLayout {...props} />} />
+      <Route path="/admin" render={props => <AdminLayout {...props} />} />
+      <Redirect to="/admin/dashboard" />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
