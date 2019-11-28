476e966a2a1144d819e880ac2e3da6644e4d766d
Wesley Hatley <whatley@lenovo.com>
2019-11-27 23:17:55 -0500

    Updates

---
+++ precache-manifest.e72677b67c9205fee66b58f95323fd96.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4443583226be5498b5faaaa3fe88e8",
+    "url": "/Paper/index.html"
+  },
+  {
+    "revision": "f139f3ad3ed4b7592206",
+    "url": "/Paper/static/css/2.9d5e3c4e.chunk.css"
+  },
+  {
+    "revision": "f9839ea07b2b1ee6c6f4",
+    "url": "/Paper/static/css/main.4ba6ecac.chunk.css"
+  },
+  {
+    "revision": "f139f3ad3ed4b7592206",
+    "url": "/Paper/static/js/2.76f1c822.chunk.js"
+  },
+  {
+    "revision": "f9839ea07b2b1ee6c6f4",
+    "url": "/Paper/static/js/main.1e7025a9.chunk.js"
+  },
+  {
+    "revision": "8d91c820d5dca882e86d",
+    "url": "/Paper/static/js/runtime~main.16e18e5a.js"
+  },
+  {
+    "revision": "c411a67eb76ab9a7ad26ca5d19b61392",
+    "url": "/Paper/static/media/agenda.c411a67e.png"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/Paper/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "80954654631af8ffb5ef4453a9caa3ec",
+    "url": "/Paper/static/media/bruno-abatti.80954654.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/Paper/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/Paper/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "af300b6e21dff8e136aa4f4acdb8dd11",
+    "url": "/Paper/static/media/daniel-olahs.af300b6e.jpg"
+  },
+  {
+    "revision": "8f9581dbf863fcb3494a8bbe0f89d6eb",
+    "url": "/Paper/static/media/david-marcu.8f9581db.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/Paper/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "8e04f24a8013fe52d0851ce14713ab2a",
+    "url": "/Paper/static/media/evernote.8e04f24a.png"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/Paper/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "ebe9884b2152a08a525fcd5054019532",
+    "url": "/Paper/static/media/image_placeholder.ebe9884b.jpg"
+  },
+  {
+    "revision": "04f0390102a515ab4bb092d34f01eed8",
+    "url": "/Paper/static/media/jan-sendereks.04f03901.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/Paper/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "e06d84cb846db2a463ac3d904ac0c362",
+    "url": "/Paper/static/media/kaci-baum-2.e06d84cb.jpg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/Paper/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/Paper/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/Paper/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/Paper/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/Paper/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "f771455e07d39b0841a3f0d4a2143c5d",
+    "url": "/Paper/static/media/placeholder.f771455e.jpg"
+  },
+  {
+    "revision": "7907591946eee451e53d97d92cc238c9",
+    "url": "/Paper/static/media/react-logo.79075919.png"
+  }
+]);
